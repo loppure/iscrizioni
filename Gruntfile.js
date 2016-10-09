3d8f6dee7376c479b70dcd5494d1b92b889f2fd0
Omar Polo <Omar Polo>
2016-10-09 20:36:53 +0200

    Grunt ha un default task (stylus + watch)

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,4 +38,6 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-stylus');
     grunt.loadNpmTasks('grunt-contrib-watch');
-}
+
+    grunt.registerTask('default', ['stylus', 'watch']);
+};
